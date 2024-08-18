c18cd6a1c7af72d1941ee041c52e890f945e91eb
jal-morales <jal.morales89@gmail.com>
2024-08-18 01:36:13 -0400

    Se configuran las vistas principales

--- app/src/main/java/com/example/appseas/LoginActivity.kt
+++ app/src/main/java/com/example/appseas/LoginActivity.kt
@@ -1,12 +1,16 @@
 package com.example.appseas
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+    private  lateinit var registrarse_btn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,20 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        registrarse_btn=findViewById(R.id.registering_user)
+
+        registrarse_btn.setOnClickListener {
+            val intent= Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Configuración del botón de iniciar sesión
+        val loginButton: Button = findViewById(R.id.inicio_App)
+        loginButton.setOnClickListener {
+            // Redirigir a la actividad que muestra activity_home.xml
+            val intent = Intent(this, InicioActivity::class.java)
+            startActivity(intent)
         }
 
 
--- app/src/main/java/com/example/appseas/RegisterActivity.kt
+++ app/src/main/java/com/example/appseas/RegisterActivity.kt
@@ -1,6 +1,8 @@
 package com.example.appseas
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,16 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Encuentra el botón por su ID
+        val volverInicioButton: Button = findViewById(R.id.VolverInicio_App)
+        volverInicioButton.setOnClickListener {
+            // Redirige a LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // Opcional: cerrar RegisterActivity si no se debe regresar a ella
+        }
     }
+
+
 }
